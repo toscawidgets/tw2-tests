a750fc6cc2727f006a355750f2d3f2be03fcd670
Ralph Bean <ralph.bean@gmail.com>
2011-05-10 01:57:20 -0400

    Warming up to testing and coverage.

--- repolist.py
+++ repolist.py
@@ -5,7 +5,8 @@ import os
 import shutil
 
 bitbucket_repos = {
-    'tw2.core' : 'toscawidgets',
+    # THIS IS UNBEARABLE.  TODO -- change to tw2.core
+    'tw2core' : 'ralphbean',
 }
 github_repos = {
     'tw2.jit' : 'ralphbean',
@@ -44,7 +45,22 @@ class Repo(object):
 
     @property
     def test_command(self):
-        return 'cd %s ; python setup.py nosetests ; cd -' % self.name
+        commands = [
+            "cd %s" % self.name,
+            "rmvirtualenv %s-venv" % self.name,
+            "mkvirtualenv --no-site-packages %s-venv" % self.name,
+            "pip install coverage",
+            "python setup.py develop",
+            "coverage run --source=tw2/ setup.py test",
+            "COV=$(coverage report | tail -1 | awk ' { print $4 } ')",
+            "coverage html",
+            "mv htmlcov ../htmlcov/htmlcov-%s" % self.name,
+            "deactivate",
+            "cd -",
+            "notify-send '%s' \"$COV\"" % self.name,
+            "echo \"%s $COV\" >> htmlcov/index.html" % self.name,
+        ]
+        return " ; ".join(commands)
 
 repos = {}
 
@@ -80,12 +96,25 @@ for k, v in custom_repos.iteritems():
 
 def destroy():
     """ rm -rf tw2* """
-    d, dirs, files = list(os.walk('/'.join(__file__.split('/')[:-1])))[0]
+    print "Destroying."
+    current_dir = '/'.join(__file__.split('/')[:-1])
+    d, dirs, files = list(os.walk(current_dir))[0]
     for d in dirs:
         if not d.startswith('tw2'):
+            print " Ignoring directory", d
+            continue
+        print " Removing", d
+        shutil.rmtree(d)
+    d, dirs, files = list(os.walk(current_dir+"/htmlcov/"))[0]
+    for d in dirs:
+        if not d.startswith('htmlcov'):
             print "Ignoring directory", d
             continue
+        d = 'htmlcov/'+d
+        print " Removing", d
         shutil.rmtree(d)
+    print "Destroyed!"
+    print "----"
 
 def clone():
     for name, repo in repos.iteritems():
