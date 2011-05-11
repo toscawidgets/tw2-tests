b2e8fa02ebf18a90c491bcd13261862bd814f45c
Ralph Bean <ralph.bean@gmail.com>
2011-05-11 11:57:38 -0500

    Uncommented stuff I accidentally commented.

--- generate-report.sh
+++ generate-report.sh
@@ -4,15 +4,16 @@ source ~/.bashrc
 workon finalize_report || mkvirtualenv --no-site-packages finalize_report
 pip install mako genshi mercurial formencode
 
-#./_repolist.py > _run-tests.sh
-#chmod +x _run-tests.sh
+./_repolist.py > _run-tests.sh
+chmod +x _run-tests.sh
 
-#./_run-tests.sh
+./_run-tests.sh
 
-#hg clone https://bitbucket.org/ralphbean/tw2.devtools || echo "Not cloning."
-#cd tw2.devtools ; python setup.py install ; cd -
+hg clone https://bitbucket.org/ralphbean/tw2.devtools || echo "Not cloning."
+cd tw2.devtools ; python setup.py install ; cd -
 ./_finalize_report.py
 deactivate
+rm -rf ~/webapps/tw2_tests/*
 cp htmlcov/index.html ~/webapps/tw2_tests/.
 cp htmlcov/threebean.css ~/webapps/tw2_tests/.
 mv htmlcov/htmlcov* ~/webapps/tw2_tests/.
