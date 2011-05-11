73a91332977c3b8b0ec8e8aa3fe65e8999c1983b
Ralph Bean <ralph.bean@gmail.com>
2011-05-10 21:23:21 -0500

    Final webfaction changes -- up and running.

--- generate-report.sh
+++ generate-report.sh
@@ -2,15 +2,14 @@
 source ~/.bashrc
 
 workon finalize_report || mkvirtualenv --no-site-packages finalize_report
-pip install mako mercurial
+pip install mako genshi mercurial formencode
 
 ./_repolist.py > _run-tests.sh
 chmod +x _run-tests.sh
 
-
 ./_run-tests.sh
 
-hg clone https://bitbucket.org/ralphbean/tw2.devtools
+hg clone https://bitbucket.org/ralphbean/tw2.devtools || echo "Not cloning."
 cd tw2.devtools ; python setup.py install ; cd -
 ./_finalize_report.py
 deactivate
